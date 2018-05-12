c7cf25204cc902d96fafe2a21552536641a70092
jamesyeobh <38290271+jamesyeobh@users.noreply.github.com>
2018-05-13 01:36:59 +0800

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,26 @@
+<?php
+	
+	require_once '/vendor/swiftmailer-5.x/lib/swift_required.php';
+	function send_mail($recipient, $subject="", $message="") {
+		if (strpos($recipient, ",") !== true) {
+			$recipient = preg_split("/[,]+/", $recipient);
+		}
+		$transport = (new Swift_SmtpTransport('in-v3.mailjet.com', 25))
+		  ->setUsername('')
+		  ->setPassword('');
+		$mailer = new Swift_Mailer($transport);
+		
+		$message = (new Swift_Message($subject))
+			->setFrom(['' => 'Webmaster'])
+			//->setTo([$recipient]) //Recipients
+			->setTo($recipient) //Recipients
+
+			//Recipients
+			->setBody($message); //Bodymessage
+			
+		if ($mailer->send($message) > 0) {
+		  return 1;
+		return 0;
+	}
+	}
+?>
